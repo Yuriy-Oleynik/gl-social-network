ef715dc8b80b46a8fb86b3aeb7384e685f66e11b
Yurii Oliinyk <yurii.oliinyk@icloud.com>
2020-11-24 15:04:13 +0200

    Initial commit

---
+++ src/main/java/glsocialnetwork/GlSocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package glsocialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlSocialNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlSocialNetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/glsocialnetwork/GlSocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package glsocialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlSocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
