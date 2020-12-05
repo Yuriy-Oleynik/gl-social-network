539e4f426019d9ad91e8a5a282e356a000516e71
Yurii Oliinyk <yurii.oliinyk@icloud.com>
2020-12-05 14:50:44 +0200

    Added swagger configuration

--- src/main/java/glsocialnetwork/GlSocialNetworkApplication.java
+++ src/main/java/glsocialnetwork/GlSocialNetworkApplication.java
@@ -1,9 +1,12 @@
 package glsocialnetwork;
 
+import glsocialnetwork.swagger.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(SwaggerConfiguration.class)
 public class GlSocialNetworkApplication {
 
 	public static void main(String[] args) {
